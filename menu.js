81761fb4a1f2e6214f9f9cdaebb948ee87020d49
Andy <andycheung83a@hotmail.com>
2024-01-23 22:40:38 +0100

    feat(desktop): Lägg till desktop-sidebar outside click-döljning
    - Lägg till funktionalitet för att dölja desktop-sidebaren när man klickar utanför den

--- menu.js
+++ menu.js
@@ -1,7 +1,13 @@
-// Side bar Toggle
+// Sidebar Toggle
 const menuButton = document.getElementById('menu-btn')
 menuButton.addEventListener('click', function () {
-	console.log('Menu button click!')
 	const sideBar = document.getElementById('sidebar')
 	sideBar.classList.toggle('sidebar-active')
+})
+
+// Hides Sidebar when you click outside it
+const sectionContainer = document.getElementById('section-container')
+sectionContainer.addEventListener('click', function () {
+	const sideBar = document.getElementById('sidebar')
+	sideBar.classList.remove('sidebar-active')
 })
