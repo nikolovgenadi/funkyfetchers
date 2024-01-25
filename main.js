e143279509de87e709e70a712f3c8acf323b24a3
Genadi Nikolov <genadi123.nik@gmail.com>
2024-01-25 16:08:09 +0100

    one api active

--- main.js
+++ main.js
@@ -3,44 +3,44 @@ import axios from "axios";
 let country = "Sweden"
 
 const apiKey = import.meta.env.VITE_API_KEY;
-const newsDataKey = import.meta.env.VITE_NEWSDATA_KEY;
-const NEWS_DATA_URL = `https://newsdata.io/api/1/news?apikey=${newsDataKey}`
 const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`
+// const newsDataKey = import.meta.env.VITE_NEWSDATA_KEY;
+// const NEWS_DATA_URL = `https://newsdata.io/api/1/news?apikey=${newsDataKey}`
 
-async function newsDataIo() {
-  try {
-    const response = await axios.get(NEWS_DATA_URL);
-    console.log(response);
+// async function newsDataIo() {
+//   try {
+//     const response = await axios.get(NEWS_DATA_URL);
+//     console.log(response);
 
-    const newsDataArticles = response.data.results;
-    const newsCont = document.querySelector(".toppnews-container");
+//     const newsDataArticles = response.data.results;
+//     const newsCont = document.querySelector(".toppnews-container");
 
-    newsDataArticles.forEach(result => {
-      if (
-        result.country &&
-        result.category &&
-        result.title &&
-        result.image_url &&
-        result.pubDate
-      ) {
-        const newsItems = document.createElement('div');
-        newsItems.innerHTML = 
-        `<h3>${result.title}</h3>
-        <img src="${result.image_url}" class="news-image" />
-        <p>${result.description}</p>
-        <a href="${result.source_url}" target="_blank">Läs mer</a>
-        <hr>`;
+//     newsDataArticles.forEach(result => {
+//       if (
+//         result.country &&
+//         result.category &&
+//         result.title &&
+//         result.image_url &&
+//         result.pubDate
+//       ) {
+//         const newsItems = document.createElement('div');
+//         newsItems.innerHTML = 
+//         `<h3>${result.title}</h3>
+//         <img src="${result.image_url}" class="news-image" />
+//         <p>${result.description}</p>
+//         <a href="${result.source_url}" target="_blank">Läs mer</a>
+//         <hr>`;
 
-        newsCont.appendChild(newsItems);
-      }
-    });
-  } catch(error) {
-    console.log(error);
-  }
-}
+//         newsCont.appendChild(newsItems);
+//       }
+//     });
+//   } catch(error) {
+//     console.log(error);
+//   }
+// }
 
 
-newsDataIo();
+// newsDataIo();
 
 async function fetchNews(url) {
     try {
@@ -73,7 +73,6 @@ async function fetchNews(url) {
       console.log("Something went wrong: " + error);
     }
 }
-
 
 fetchNews(url);	
 
