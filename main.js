e5b956edd156a17d8ea1d36e1563ed2888d47918
Genadi Nikolov <genadi123.nik@gmail.com>
2024-01-25 16:07:04 +0100

    news data api fixed

--- main.js
+++ main.js
@@ -3,41 +3,44 @@ import axios from "axios";
 let country = "Sweden"
 
 const apiKey = import.meta.env.VITE_API_KEY;
-// const NEWSDATA_KEY = import.meta.env.VITE_NEWSDATA_KEY;
-// const NEWS_DATA_URL = `https://newsdata.io/api/1/news?apikey=${NEWSDATA_KEY}`
+const newsDataKey = import.meta.env.VITE_NEWSDATA_KEY;
+const NEWS_DATA_URL = `https://newsdata.io/api/1/news?apikey=${newsDataKey}`
 const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`
 
-// news data fetch
-// async function newsDataIo() {
-//   try {
-//     const response = await axios.get(NEWS_DATA_URL);
-//     console.log(response);
+async function newsDataIo() {
+  try {
+    const response = await axios.get(NEWS_DATA_URL);
+    console.log(response);
 
-//     const newsDataArticles = response.data.results;
-//     newsDataArticles.forEach(result)
-//     if (
-//       results.country &&
-//       results.category &&
-//       results.title &&
-//       results.image_url &&
-          // results.pubDate
-//     ) {
-        // const newsCont = document.querySelector(".toppnews-container");
-        // const newsItems = document.createElement('div');
-        // newsItems.innerHTML = 
-        // `<h3>${results.title}</h3>
-        // <img src="${results.image_url}" class="news-image" />
-        // <p>${results.description}</p>
-        // <a href="${results.source_url}" target="_blank">Läs mer</a>
-        // <hr>`;
-//     }
-//   }
-//   catch(error) {
-//     console.log(error);
-//   }
-// }
+    const newsDataArticles = response.data.results;
+    const newsCont = document.querySelector(".toppnews-container");
 
-// newsDataIo();
+    newsDataArticles.forEach(result => {
+      if (
+        result.country &&
+        result.category &&
+        result.title &&
+        result.image_url &&
+        result.pubDate
+      ) {
+        const newsItems = document.createElement('div');
+        newsItems.innerHTML = 
+        `<h3>${result.title}</h3>
+        <img src="${result.image_url}" class="news-image" />
+        <p>${result.description}</p>
+        <a href="${result.source_url}" target="_blank">Läs mer</a>
+        <hr>`;
+
+        newsCont.appendChild(newsItems);
+      }
+    });
+  } catch(error) {
+    console.log(error);
+  }
+}
+
+
+newsDataIo();
 
 async function fetchNews(url) {
     try {
@@ -45,16 +48,15 @@ async function fetchNews(url) {
       console.log(response);
 
       const newsArticles = response.data.articles;
-const newsCont = document.querySelector(".toppnews-container");
-
-newsArticles.forEach(function (article) {
-  if (
-    article.title &&
-    article.urlToImage &&
-    article.description &&
-    article.url &&
-    article.publishedAt &&
-  ) {
+      const newsCont = document.querySelector(".toppnews-container");
+      newsArticles.forEach(function (article) {
+      if (
+        article.title &&
+        article.urlToImage &&
+        article.description &&
+        article.url &&
+        article.publishedAt
+      ) {
   const newsItem = document.createElement('div');
   newsItem.innerHTML = `
     <h3>${article.title}</h3>
